a2c90b2324faa0eb68af2d3c6cec8fc9365e94c6
ranjit-27 <ranjithmadugula6@gmail.com>
2025-09-19 12:58:55 +0530

    updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,7 @@
 const router=require("express").Router();
 const User=require("../models/user.model");
-const bcrypt=require("bcryptjs");
-const jwt=require("jsonwebtoken");
-router.route("/signup ").post(async(req,res)=>{
+
+router.route("/signup").post(async(req,res)=>{
     const {username,password}=req.body;
     if(!username || !password){
         return res.status(400).json({message:"Username and password are required"});
